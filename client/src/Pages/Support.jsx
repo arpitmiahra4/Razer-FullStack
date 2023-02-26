729a5f693750043ff3e5fdd9868f3c487be594b3
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-02-27 03:09:18 +0530

    added: some update

--- client/src/Pages/Support.jsx
+++ client/src/Pages/Support.jsx
@@ -33,23 +33,87 @@ const Support = () => {
       <Box bg={"#1a1a1a"} py="5%">
         <Flex justifyContent={"center"} gap="20px">
           <Box border={"1px solid white"} w={"30%"} py="25px" pl="20px">
-            <Text fontWeight={"bold"} fontSize={25} mb="5px">Popular Support Videos</Text>
+            <Text fontWeight={"bold"} fontSize={25} mb="5px">
+              Popular Support Videos
+            </Text>
             <UnorderedList>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>View the latest support videos</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Set up the Razer Katana</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Resolve Razer Synapse when it does not start</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Troubleshoot and resolve headset mic issues</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Detect Razer devices in Synapse 3</ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                View the latest support videos
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Set up the Razer Katana
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Resolve Razer Synapse when it does not start
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Troubleshoot and resolve headset mic issues
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Detect Razer devices in Synapse 3
+              </ListItem>
             </UnorderedList>
           </Box>
           <Box border={"1px solid white"} w={"30%"} py="25px" pl="20px">
-            <Text fontWeight={"bold"} fontSize={25} mb="5px">Popular Support Videos</Text>
+            <Text fontWeight={"bold"} fontSize={25} mb="5px">
+              Popular Support Videos
+            </Text>
             <UnorderedList>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Razer online services (self-service tools)</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Razer Synapse 3 FAQs</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Help with 7.1 Surround Sound activation</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Firmware updates for Razer Peripherals</ListItem>
-              <ListItem textAlign={"left"} _hover={{color:"#44d62c",cursor:"pointer"}}  fontSize={16}>Device is not detected by Razer Synapse</ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Razer online services (self-service tools)
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Razer Synapse 3 FAQs
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Help with 7.1 Surround Sound activation
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Firmware updates for Razer Peripherals
+              </ListItem>
+              <ListItem
+                textAlign={"left"}
+                _hover={{ color: "#44d62c", cursor: "pointer" }}
+                fontSize={16}
+              >
+                Device is not detected by Razer Synapse
+              </ListItem>
             </UnorderedList>
           </Box>
         </Flex>
